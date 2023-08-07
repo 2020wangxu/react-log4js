a7b63bc1d9c3e3ceec84d17059a5142dd4e9783e
Tom_wang <66865213+2020wangxu@users.noreply.github.com>
2023-08-07 12:10:05 +0800

    Create Log4Js.ts

---
+++ config/Log4Js.ts
@@ -0,0 +1,99 @@
+// Log4Js.ts
+ 
+import log4js from "log4js";
+ 
+log4js.configure({
+  appenders: {
+    logFile: {
+      type: "dateFile",
+      filename: "./logs/logFile", // 你要写入日志文件的路径
+      alwaysIncludePattern: true, // 将模式包含在当前日志文件的名称及其备份中
+      pattern: "yyyy-MM-dd-hh.log",
+      encoding: "utf-8",
+      maxLogSize: 10240, // 文件最大存储空间
+    },
+    logErrorFile: {
+      type: "dateFile",
+      filename: "./logs/logFile", // 你要写入日志文件的路径
+      alwaysIncludePattern: true, // 将模式包含在当前日志文件的名称及其备份中
+      pattern: "yyyy-MM-dd-hh.error.log",
+      encoding: "utf-8",
+      maxLogSize: 10240, // 文件最大存储空间
+    },
+    apiLogFile: {
+      type: "dateFile",
+      filename: "./logs/apiLogFile", // 你要写入日志文件的路径
+      alwaysIncludePattern: true, // 将模式包含在当前日志文件的名称及其备份中
+      pattern: "yyyy-MM-dd-hh.log",
+      encoding: "utf-8",
+      maxLogSize: 10240, // 文件最大存储空间
+    },
+    apiLogErrorFile: {
+      type: "dateFile",
+      filename: "./logs/apiLogFile", // 你要写入日志文件的路径
+      alwaysIncludePattern: true, // 将模式包含在当前日志文件的名称及其备份中
+      pattern: "yyyy-MM-dd-hh.error.log",
+      encoding: "utf-8",
+      maxLogSize: 10240, // 文件最大存储空间
+    },
+    logConsole: {
+      type: "console",
+    },
+  },
+  categories: {
+    default: {
+      appenders: ["logFile"],
+      level: "all",
+    },
+    info: {
+      appenders: ["logFile"],
+      level: "all",
+    },
+    error: {
+      appenders: ["logErrorFile"],
+      level: "error",
+    },
+    apiInfo: {
+      appenders: ["apiLogFile"],
+      level: "all",
+    },
+    apiError: {
+      appenders: ["apiLogErrorFile"],
+      level: "error",
+    },
+    console: {
+      appenders: ["logConsole"],
+      level: log4js.levels.ALL.levelStr,
+    }
+  },
+});
+ 
+export default {
+  info: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "info": "console")
+    logger.info(msg, ...args)
+  },
+  debug: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "info": "console")
+    logger.debug(msg, ...args)
+  },
+  error: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "error": "console")
+    logger.error(msg, ...args)
+  },
+  apiInfo: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "apiInfo": "console")
+    logger.info(msg, ...args)
+  },
+  apiDebug: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "apiInfo": "console")
+    logger.debug(msg, ...args)
+  },
+  apiError: function(msg: string, ...args: any[]) {
+    const logger = log4js.getLogger(process.env.mode === "production" ? "apiError": "console")
+    logger.error(msg, ...args)
+  },
+}
+————————————————
+版权声明：本文为CSDN博主「WangminzeSpk」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/Sparkler123/article/details/111871024
